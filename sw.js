b1b78afa0908b48a87450238a79baa1bff8f5c3f
zhen-droid00 <putusendianasaputra@gmail.com>
2026-08-07 21:20:53 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gapura-smart-v1';
+const CACHE_NAME = 'gapura-smart-v2';
 const urlsToCache = [
   './',
   './index.html',
@@ -7,14 +7,32 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting(); // Memaksa update langsung aktif
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// STRATEGI NETWORK-FIRST (Ambil web terbaru dulu, kalau offline baru pakai memori)
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request).then(response => {
+      return caches.open(CACHE_NAME).then(cache => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
